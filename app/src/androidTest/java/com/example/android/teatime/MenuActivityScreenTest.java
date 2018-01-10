29713f3bb7d590f9e056bd70a32de688c50d0e90
Yuri Levenhagen <yurileven@gmail.com>
2018-01-10 13:41:08 -0200

    Add basic tests for the tea adapter

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -7,11 +7,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 /**
  * Created by Yuri Levenhagen on 2018-01-10 as part
@@ -34,34 +36,22 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  */
 
 @RunWith(AndroidJUnit4.class)
-public class OrderActivityBasicTest {
+public class MenuActivityScreenTest {
 
-    @Rule public ActivityTestRule<OrderActivity> mActivityTestRule
-            = new ActivityTestRule<>(OrderActivity.class);
+    private static final int GREEN_TEA_INDEX = 1;
 
-    @Test
-    public void clickIncrementButton_ChangesQuantityAndCost() {
-        onView(withId(R.id.increment_button)).perform(click());
-
-        onView(withId(R.id.quantity_text_view))
-                .check(matches(withText("1")));
-
-        onView(withId(R.id.cost_text_view))
-                .check(matches(withText("$5.00")));
-    }
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule
+            = new ActivityTestRule<>(MenuActivity.class);
 
     @Test
-    public void clickDecrementButton_ReturnsQuantityAndValueToZero() {
-        // Increment the values
-        onView(withId(R.id.increment_button)).perform(click());
+    public void clickGridView_StartsOrderActivityWithTheSelectedTypeOfTea() {
+        onData(anything())
+                .inAdapterView(withId(R.id.tea_grid_view))
+                .atPosition(GREEN_TEA_INDEX)
+                .perform(click());
 
-        // Then decrement them, getting back to zero
-        onView(withId(R.id.decrement_button)).perform(click());
-
-        onView(withId(R.id.quantity_text_view))
-                .check(matches(withText("0")));
-
-        onView(withId(R.id.cost_text_view))
-                .check(matches(withText("$0.00")));
+        onView(withId(R.id.tea_name_text_view))
+                .check(matches(withText("Green Tea")));
     }
 }
